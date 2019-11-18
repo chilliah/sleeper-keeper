75f10fd516758dcc9b2a5b681d59ba2e0c9d3aef
chilliah <41967233+chilliah@users.noreply.github.com>
2019-11-18 11:43:40 -0600

    Update keeperwebpage.py

--- keeperwebpage.py
+++ keeperwebpage.py
@@ -16,5 +16,16 @@ def main():
     return render_template('content.html', text=content)
 
 
+@app.route('/debug')
+def debug():
+    """ Base URL route used only for debugging purposes and to make sure that the webserver is running """
+
+    main_program('chilliah', False, False, None, False)
+
+    text = open('final_keepers.txt', 'r+')
+    content = text.read()
+    text.close()
+    return render_template('content.html', text=content)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
