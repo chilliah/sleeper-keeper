dd0868fb0d5d1381051e945f8cd23eaf4253f3e1
chilliah <jjordan141@gmail.com>
2024-12-30 03:16:46 -0600

    swap to prod
    swap to prod

--- yafl_keeperwebpage.py
+++ yafl_keeperwebpage.py
@@ -467,8 +467,8 @@ if __name__ == "__main__":
     # app.run(debug=True)
 
     # Cloud debug flask
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    #app.run(host='127.0.0.1', port=8080, debug=True)
 
     #Production run
-    #app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
 
